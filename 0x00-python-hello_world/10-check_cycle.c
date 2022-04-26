3abe8b6addff2f6498e30ebb4d2b6a55fa7c11f3
Your Name <yohel.cruz.espinoza@gmail.com>
2022-04-25 18:50:19 -0700

    Added 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,7 +2,7 @@
 
 /**
  * check_cycle - checks if a singly linked list has a cycle in it
- * list: linked list to check
+ * @list: linked list to check
  *
  * Return: 0 if there is no cycle, 1 if there is a cycle
  */
