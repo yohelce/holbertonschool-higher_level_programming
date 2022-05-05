a22ce17ac6b7174ce0bdde5dc041e9a335d4d3c6
Your Name <yohel.cruz.espinoza@gmail.com>
2022-05-04 17:05:19 -0700

    Added 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/python3
+def roman_to_int(roman_string):
+    rs = roman_string
+    if roman_string is None:
+        return 0
+    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+
+    for i in range(len(rs)):
+        if i + 1 < len(rs) and dic[rs[i]] < dic[rs[i + 1]]:
+            result -= dic[rs[i]]
+        else:
+            result += dic[rs[i]]
+    return result
