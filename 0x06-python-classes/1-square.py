038afea63d6efb7002598c3d2fee3795c876ff49
Your Name <yohel.cruz.espinoza@gmail.com>
2022-05-17 17:28:13 -0700

    Added 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Empty Class Square"""
+
+
+class Square:
+    """Represents a square.
+    Private instance attribute: size
+    Instantiation with size (no type/value verification)
+    """
+
+    def __init__(self, size):
+        """This is a private instance attribute"""
+        self.__size = size
