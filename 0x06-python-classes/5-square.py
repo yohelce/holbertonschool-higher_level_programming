e7f2806e4b8eabe284241faac6ca7934fbc1d593
Your Name <yohel.cruz.espinoza@gmail.com>
2022-05-17 21:17:52 -0700

    Added 5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Empty Class Square"""
+
+
+class Square:
+    """Represents a square.
+    Private instance attribute: size
+    Instantiation with size (no type/value verification)
+    """
+
+    def __init__(self, size=0):
+        """Initialize the data defining size of square"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Public instance method that returns area of a square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """"Public instance that prints the square with the character #"""
+        for i in range(self.__size):
+            print("#" * self.__size)
+        if self.__size == 0:
+            print()
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.value = value
+
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self._Square__size = value
