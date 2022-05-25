77fe0c912195de0742f05c3caaaa5a09f7dff5a4
Your Name <yohel.cruz.espinoza@gmail.com>
2022-05-25 09:41:32 -0700

    Pycodestyle fixed 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,14 +3,14 @@
 
 
 def text_indentation(text):
-    """Prints a text with 2 new lines after each 
+    """Prints a text with 2 new lines after each
     of these characters: ., ? and :
     """
 
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     delim = ".?:"
-    for element in delim: 
-        text = [each_line.strip() for each_line in text.split(element)]
+    for element in delim:
+        text = [each_line.strip(" ") for each_line in text.split(element)]
         text = (element + "\n\n").join(text)
-    print(f"{text}", end= '')
+    print(f"{text}", end='')
