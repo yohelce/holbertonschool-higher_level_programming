e671fa2f6c5fd43be14189f2addf643b69a148c3
Your Name <yohel.cruz.espinoza@gmail.com>
2022-06-08 15:52:03 -0700

    Added models/base.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Module base """
+
+
+class Base:
+    """Class with:
+    Private class attribute: __nb_objects
+    """
+
+    __nb_objects = 0
+    
+    def __init__(self, id=None):
+        """Initialization of a Base instance.
+        Args:
+            - id: id of the instance
+        """
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
