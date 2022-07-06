1758e5d66d3732087d1710574b9f2d54f36ca25d
Your Name <yohel.cruz.espinoza@gmail.com>
2022-07-05 22:37:20 -0700

    Added 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -4,6 +4,10 @@ CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE hbtn_0c_0.first_table
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE hbtn_0c_0.first_table
 MODIFY name VARCHAR(256)
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
