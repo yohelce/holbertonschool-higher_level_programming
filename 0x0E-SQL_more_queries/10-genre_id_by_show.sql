b4e1c2a4b87d2195e6b2938062d4a526d148f130
Your Name <yohel.cruz.espinoza@gmail.com>
2022-07-06 22:02:59 -0700

    Added 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,4 +3,4 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres
      ON tv_show_genres.show_id = tv_shows.id
-ORDER BY tv_shows.title, tv_show_genres.genre_idi ASC;
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
