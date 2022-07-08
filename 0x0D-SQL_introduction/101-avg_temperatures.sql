4cd2d77a400d25740237f352b4b37f39c2b260be
Your Name <yohel.cruz.espinoza@gmail.com>
2022-07-07 20:19:40 -0700

    Added 101-avg_temperatures.sql

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,4 @@
+-- Import in hbtn_0c_0 database this table dump and display
+-- temperature (Fahrenheit) by city ordered by temperature (descending)
+SELECT city, AVG(value) AS 'avg_temp' FROM temperatures
+GROUP BY city ORDER BY avg_temp DESC;
