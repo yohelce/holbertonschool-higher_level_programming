fa8fb9108eb4d340c319fb461e3d4370a0d60de6
yohelce <yohel.cruz.espinoza@gmail.com>
2022-08-02 00:00:09 -0500

    Added 6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const square = require('./5-square');
+
+class Square extends square {
+  charPrint (c) {
+    if (typeof c === 'undefined') {
+      c = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
