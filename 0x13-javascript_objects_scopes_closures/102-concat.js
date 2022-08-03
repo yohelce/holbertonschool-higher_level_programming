e527944a7f30e117d891ae1b6556602598685b0d
yohelce <yohel.cruz.espinoza@gmail.com>
2022-08-02 23:59:13 -0500

    Added 102-concat.js

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,7 @@
+#!/usr/local/bin/node
+const { argv } = require('process');
+const fs = require('fs');
+
+const file1 = fs.readFileSync(argv[2]);
+const file2 = fs.readFileSync(argv[3]);
+fs.writeFileSync(argv[4], file1 + file2);
