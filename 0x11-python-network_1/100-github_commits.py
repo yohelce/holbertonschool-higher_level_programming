9557841b749f94196af4706a0a7bd46ef1042142
yohelce <yohel.cruz.espinoza@gmail.com>
2022-08-20 16:14:17 -0500

    Fixed pycodestyle

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,9 +6,12 @@ import requests
 if __name__ == "__main__":
     repo = sys.argv[1]
     owner = sys.argv[2]
-    req =  requests.get('https://api.github.com/repos/{}/{}/commits'
-                        .format(owner, repo))
+    req = requests.get('https://api.github.com/repos/{}/{}/commits'
+                       .format(owner, repo))
     my_dict = req.json()
     if req.status_code == 200:
         for elem in my_dict[:10]:
-            print('{}: {}'.format(elem.get('sha'), elem.get('commit').get('author').get('name')))
+            print('{}: {}'.format(elem.get('sha'),
+                                  elem.get('commit')
+                                      .get('author')
+                                      .get('name')))
