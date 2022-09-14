2971a613f0c7b771fb16b9eb556cee52557ab998
yohelce <yohel.cruz.espinoza@gmail.com>
2022-09-13 23:46:48 -0500

    Added 4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const axios = require('axios').default;
+const url = process.argv[2];
+
+axios.get(url)
+  .then(function (response) {
+    let count = 0;
+    for (const film of response.data.results) {
+      for (const people of film.characters) {
+        if (people === 'https://swapi-api.hbtn.io/api/people/18/') {
+          count += 1;
+        }
+      }
+    }
+    console.log(count);
+  })
+  .catch(function (err) {
+    console.log(err.response.status);
+  });
